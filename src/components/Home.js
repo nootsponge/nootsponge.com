2c5892decc926df398eca6d195022167073a7622
Andrew <andrew@nootsponge.com>
2022-04-15 12:20:25 -0500

    Initial v2 commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <>
+      <img src="https://cdn.n00t.io/file/ntspng/pfp/sunny_bp_200.gif" alt="Loading" />
+      <span class="beeg no-margin">NootSponge</span><br />
+      <span class="smol">Backend Developer // I suck at UI</span> 
+      <a href="mailto:andrew@nootsponge.com" target="_blank" rel="noreferrer">CONTACT</a>&nbsp;|&nbsp;
+      <a href="https://twitter.com/nootsponge" target="_blank" rel="noreferrer">TWITTER</a>&nbsp;|&nbsp;
+      <a href="https://steamcommunity.com/id/nootspongeborb" target="_blank" rel="noreferrer">STEAM</a>&nbsp;|&nbsp;
+      <b>DISCORD: Andrew#2659</b>
+    </>
+  )    
+}
+
+export default Home;
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <>
+          <span class="beeg"><b>404</b> PAGE<b>NOT</b>FOUND</span>
+          <p>Try again?</p>
+        </>
+    )
+}
+
+export default NotFound;
