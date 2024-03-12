5edbb590fd1d458cd376d74c28de923c77225584
zero <hello@enumerate.dev>
2024-03-11 21:14:14 -0500

    A lot of things

---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    "bg-discord",
+    "bg-telegram",
+    "bg-twitch",
+    "bg-mastodon",
+    "bg-steam",
+    "bg-black",
+  ],
+  theme: {
+    fontFamily: {
+      mono: ['"Monaspace Neon"', ...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        discord: "#5865F2",
+        telegram: "#3FAEE8",
+        twitch: "#9146FF",
+        mastodon: "#6364FF",
+        steam: "#171a21",
+        gold: "#e7da48",
+      },
+      backgroundImage: {
+        "circle-gradient":
+          "radial-gradient(circle at center, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
