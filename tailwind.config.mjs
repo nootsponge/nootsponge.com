86a7bdf48aa1275272e6f902be70eb076d5594b4
NootSponge <hello@nootsponge.com>
2025-10-08 17:47:10 +0000

    Update meta, socials, desc.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,33 +3,34 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  safelist: [
-    "bg-discord",
-    "bg-telegram",
-    "bg-twitch",
-    "bg-mastodon",
-    "bg-steam",
-    "bg-black",
-  ],
-  theme: {
-    fontFamily: {
-      mono: ['"Monaspace Neon"', ...defaultTheme.fontFamily.mono],
-    },
-    extend: {
-      colors: {
-        discord: "#5865F2",
-        telegram: "#3FAEE8",
-        twitch: "#9146FF",
-        mastodon: "#6364FF",
-        steam: "#171a21",
-        gold: "#e7da48",
-      },
-      backgroundImage: {
-        "circle-gradient":
-          "radial-gradient(circle at center, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	safelist: [
+		"bg-discord",
+		"bg-telegram",
+		"bg-twitch",
+		"bg-mastodon",
+		"bg-steam",
+		"bg-black",
+		"bg-[#ac98ff]",
+	],
+	theme: {
+		fontFamily: {
+			mono: ['"Monaspace Neon"', ...defaultTheme.fontFamily.mono],
+		},
+		extend: {
+			colors: {
+				discord: "#5865F2",
+				telegram: "#3FAEE8",
+				twitch: "#9146FF",
+				mastodon: "#6364FF",
+				steam: "#171a21",
+				gold: "#e7da48",
+			},
+			backgroundImage: {
+				"circle-gradient":
+					"radial-gradient(circle at center, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
